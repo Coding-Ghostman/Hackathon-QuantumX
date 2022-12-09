8af2d90e8aa2fe03a5b42f1e18880c6e4eca1348
SharathRam29 <sharath29.ashok@gmail.com>
2022-12-09 08:25:06 +0530

    This is the HACKATHON QUANTUMX---1st commit

--- python_login/main.py
+++ python_login/main.py
@@ -5,18 +5,16 @@ from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
 
-app = Flask(__name__)
-# Change this to your secret key (can be anything, it's for extra protection)
-app.secret_key = 'your secret key'
-
-# Enter your database connection details below
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'pythonlogin'
-
-# Intialize MySQL
-mysql = MySQL(app)
+if __name__ == '__main__':
+    app = Flask(__name__)# Change this to your secret key (can be anything, it's for extra protection)
+    app.secret_key = 'your secret key'
+    # Enter your database connection details below
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = ''
+    app.config['MYSQL_DB'] = 'pythonlogin'
+    # Intialize MySQL
+    mysql = MySQL(app)
 
 
 # http://localhost:5000/pythonlogin/ - the following will be our login page, which will use both GET and POST requests
@@ -153,4 +151,7 @@ def admin_profile():
         return render_template('admin_profile.html', account=account)
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
+
+
+
 
