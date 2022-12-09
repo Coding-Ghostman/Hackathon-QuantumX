a386b93990c905d94e4c0b6d96613c9d3a710336
SharathRam29 <sharath29.ashok@gmail.com>
2022-12-09 08:30:54 +0530

    This is a commit message

--- python_login/main.py
+++ python_login/main.py
@@ -5,16 +5,17 @@ from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import re
 
-if __name__ == '__main__':
-    app = Flask(__name__)# Change this to your secret key (can be anything, it's for extra protection)
-    app.secret_key = 'your secret key'
-    # Enter your database connection details below
-    app.config['MYSQL_HOST'] = 'localhost'
-    app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = ''
-    app.config['MYSQL_DB'] = 'pythonlogin'
-    # Intialize MySQL
-    mysql = MySQL(app)
+app = Flask(__name__)# Change this to your secret key (can be anything, it's for extra protection)
+app.secret_key = 'your secret key'
+
+# Enter your database connection details below
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'pythonlogin'
+
+# Intialize MySQL
+mysql = MySQL(app)
 
 
 # http://localhost:5000/pythonlogin/ - the following will be our login page, which will use both GET and POST requests
